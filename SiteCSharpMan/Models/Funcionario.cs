be2fb5e655c3dd09d045c6c262879c5a43356e05
willian <touchablesystems@outlook.com>
2018-03-30 16:49:46 -0300

    Carga inicial

---
+++ SiteCSharpMan/Models/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using SiteCSharpMan.PersonalAttributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteCSharpMan.Models
+{
+    public class Funcionario
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        [MeuAtributoNome]
+        public int Idade { get; set; }
+    }
+}
---
+++ SiteCSharpMan/PersonalAttributes/MeuAtributoNome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SiteCSharpMan.PersonalAttributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class MeuAtributoNome : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int valor = 0;
+
+            int.TryParse(value.ToString(), out valor);
+
+            if (valor <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+
+        public override string FormatErrorMessage(string name)
+        {
+            return string.Format("O campo {0} está errado", name);
+        }
+    }
+}
